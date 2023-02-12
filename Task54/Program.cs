4b5353c4802b0d4d2c1e1ca5fe9ef5b9f00f763f
Nailya <n.murt@yandex.ru>
2023-02-12 19:20:17 +0300

    Task 54

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -8,3 +8,60 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+
+using System;
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.Write("Введите количество строк: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите количество столбцов: ");
+        int y = Convert.ToInt32(Console.ReadLine());
+        SortRowArray(x, y);
+    }
+
+    static int SortRowArray(int x, int y)
+    {
+        Random rand = new Random();
+        int[,] array = new int[x, y];
+        int[,] sortarray = new int[x, y];
+        int[] rowarray = new int[y];
+        Console.WriteLine("Массив: ");
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                array[i, j] = rand.Next(0, 10);
+                Console.Write($"{array[i, j]} ");
+            }
+            Console.WriteLine();
+            for (int j = 0; j < y; j++)
+            {
+                rowarray[j] = array[i, j];
+            }
+            Array.Sort(rowarray);
+            Array.Reverse(rowarray);
+            for (int j = 0; j < y; j++)
+            {
+                sortarray[i, j] = rowarray[j];
+            }
+
+        }
+        Console.WriteLine();
+        Console.WriteLine($"Отсортированный по строкам массив: ");
+
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                Console.Write($"{sortarray[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+
+        return 1;
+    }
+
+}
